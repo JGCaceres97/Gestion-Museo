9cda9a708e5d06dd5090cac5f6577b61e584460c
Joseph Cáceres <jgcaceres97@gmail.com>
2019-10-30 02:21:56 +0000

    Rutas agregadas.

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,12 @@ app.use(express.json());
 
 // Routes
 app.use(require('./routes/auth'));
+app.use(require('./routes/bitacora'));
+app.use(require('./routes/estados'));
+app.use(require('./routes/etiquetas'));
+app.use(require('./routes/horarios'));
 app.use(require('./routes/libros'));
+app.use(require('./routes/roles'));
 app.use(require('./routes/solicitudes'));
 
 module.exports = app;
