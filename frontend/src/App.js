b8957838102833be80b7362e8a0757f3401e4fa3
Joseph Cáceres <jgcaceres97@gmail.com>
2019-11-09 13:11:46 -0600

    Innecesarios removidos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 
 import AddLibro from './components/AddLibro';
 import CrearSolicitud from './components/CreateSolicitud';
