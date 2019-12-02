d652f45c4c84212578c8d9dbba423ff8e060d8e8
Joseph Cáceres <jgcaceres97@gmail.com>
2019-12-02 00:02:34 -0600

    Manejo de envío de correos

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  address: '35.185.124.104',
   port: process.env.PORT || 4000,
   db: process.env.MONGODB_URI || 'mongodb://mongo:27017/CentrosCulturales',
-  secretKey: process.env.SECRET_KEY || 'Wh4tAr3Y0uLo0K1nGF0r'
-}
+  secretKey: process.env.SECRET_KEY || 'Wh4tAr3Y0uLo0K1nGF0r',
+  emailUser: process.env.EMAIL_ADDRESS,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  adminPassword: process.env.ADMIN_PASSWORD || 'admin'
+};
